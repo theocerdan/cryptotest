216cf5a872e32b0b2f9e8823ee249376828be9fd
theocerdan <tcerdan.pro@gmail.com>
2025-01-24 13:32:40 +0100

    new test

--- test/test.js
+++ test/test.js
@@ -15,17 +15,11 @@ describe("Token contract", function () {
         expect(await hardhatToken.balanceOf(toto.address)).to.equal(500_000);
     });
 
-    it("Can transfer token and check the balance", async function () {
+    it("Total supply is 1 000 000", async function () {
         const [owner, toto] = await ethers.getSigners();
 
         const hardhatToken = await ethers.deployContract("Token");
 
-        expect(await hardhatToken.balanceOf(owner.address)).to.equal(1_000_000);
-        expect(await hardhatToken.balanceOf(toto.address)).to.equal(0);
-
-        await hardhatToken.transfer(toto.address, 500_000);
-
-        expect(await hardhatToken.balanceOf(owner.address)).to.equal(500_000);
-        expect(await hardhatToken.balanceOf(toto.address)).to.equal(500_000);
+        expect(await hardhatToken.totalSupply()).to.equal(1_000_000);
     });
 });
